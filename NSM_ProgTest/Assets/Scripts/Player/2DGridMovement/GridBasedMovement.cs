5b586b7c7429e531eab71f62786ed8fea26dba01
Riccardo <r.dec@outlook.com>
2023-07-03 22:02:09 +0200

    refactoring movement + add some sprites/ images

--- NSM_ProgTest/Assets/Scripts/Player/2DGridMovement/GridBasedMovement.cs
+++ NSM_ProgTest/Assets/Scripts/Player/2DGridMovement/GridBasedMovement.cs
@@ -9,10 +9,26 @@ public class GridBasedMovement : MonoBehaviour
 {
     [Header("movement speed :")]
     [SerializeField]
+    [Range(0.5f, 10f)]
     private float speed;
+    [Header("movement State:")]
+    [SerializeField]
+    private Inputs inputsPlayer;
+
+    [Header("player Pos :")]
     [SerializeField]
     private Transform pointToReach;
     private Animator animator;
+    [Header("Fog Of War :")]
+    [SerializeField]
+    private FogOfWar fog_Class;
+    [SerializeField]
+    private Transform fogOfWar;
+    [SerializeField]
+    [Range(0, 5)]
+    private float sightDistance;
+    [SerializeField]
+    private float checkInterval;
 
     [SerializeField]
     private LayerMask whatisWall;
@@ -24,8 +40,13 @@ public class GridBasedMovement : MonoBehaviour
     {
         animator = GetComponent<Animator>();
     }
-    private void Start() => pointToReach.parent = null;
+    private void Start()
+    {
+        StartCoroutine(CheckFogOfWar(checkInterval));
+        fogOfWar.localScale = new Vector2(sightDistance, sightDistance) * 10f;
 
+        pointToReach.parent = null;
+    }
     private void Update()
     {
         SetInputs();
@@ -48,6 +69,15 @@ public class GridBasedMovement : MonoBehaviour
                 if (!CheckIfWallOnY())
                     pointToReach.position += new Vector3(0f, yInput, 0f);
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+            inputsPlayer = Inputs.top;
+        if (Input.GetKeyDown(KeyCode.D))
+            inputsPlayer = Inputs.right;
+        if (Input.GetKeyDown(KeyCode.S))
+            inputsPlayer = Inputs.bottom;
+        if (Input.GetKeyDown(KeyCode.A))
+            inputsPlayer = Inputs.left;
     }
     private Collider2D CheckIfWallOnX()
     {
@@ -60,7 +90,16 @@ public class GridBasedMovement : MonoBehaviour
     private void AnimatorSetUp()
     {
         animator.SetFloat("Horizontal", xInput);
-        animator.SetFloat("Vertical",yInput);
-        animator.SetFloat("Speed", new Vector2(xInput,yInput).sqrMagnitude);
+        animator.SetFloat("Vertical", yInput);
+        animator.SetFloat("Speed", new Vector2(xInput, yInput).sqrMagnitude);
+    }
+    private IEnumerator CheckFogOfWar(float checkInterval)
+    {
+        while (true)
+        {
+            fog_Class.MakeHole(transform.position, sightDistance);
+            yield return new WaitForSeconds(checkInterval);
+        }
     }
 }
+public enum Inputs { top, right, left, bottom }
