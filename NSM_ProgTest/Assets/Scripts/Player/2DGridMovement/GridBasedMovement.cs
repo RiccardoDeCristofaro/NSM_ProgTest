d1a7fb2422f70fb2554afddcf8284a582af30abd
Riccardo <r.dec@outlook.com>
2023-07-04 20:23:45 +0200

    fire arrow rotation first implementation

--- NSM_ProgTest/Assets/Scripts/Player/2DGridMovement/GridBasedMovement.cs
+++ NSM_ProgTest/Assets/Scripts/Player/2DGridMovement/GridBasedMovement.cs
@@ -18,7 +18,9 @@ public class GridBasedMovement : MonoBehaviour
     [Header("player Pos :")]
     [SerializeField]
     private Transform pointToReach;
+
     private Animator animator;
+    [Header("Player Mask:")]
     [SerializeField]
     [Range(0, 5)]
     private float sightDistance;
@@ -28,11 +30,18 @@ public class GridBasedMovement : MonoBehaviour
     [SerializeField]
     private LayerMask whatisWall;
 
+    [SerializeField]
+    private Transform shootPos;
+    [SerializeField]
+    private Camera mainCam;
     private float xInput;
     private float yInput;
+    private Vector2 move;
+    private Rigidbody2D rigidbodyPlayer;
 
     private void Awake()
     {
+        rigidbodyPlayer = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
     private void Start()
@@ -44,11 +53,16 @@ public class GridBasedMovement : MonoBehaviour
         SetInputs();
         AnimatorSetUp();
     }
+    private void FixedUpdate()
+    {
+       SetRotationOfArrow();
+    }
     private void SetInputs()
     {
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
 
+        move = new Vector2 (yInput, xInput);
         transform.position = Vector3.MoveTowards(transform.position, pointToReach.position, 1f * speed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, pointToReach.position) <= 0.05f)
@@ -62,14 +76,18 @@ public class GridBasedMovement : MonoBehaviour
                     pointToReach.position += new Vector3(0f, yInput, 0f);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))      
             inputsPlayer = Inputs.top;
-        if (Input.GetKeyDown(KeyCode.D))
-            inputsPlayer = Inputs.right;
-        if (Input.GetKeyDown(KeyCode.S))
-            inputsPlayer = Inputs.bottom;
-        if (Input.GetKeyDown(KeyCode.A))
-            inputsPlayer = Inputs.left;
+                             
+        if (Input.GetKeyDown(KeyCode.S))        
+            inputsPlayer = Inputs.top;
+                    
+        if (Input.GetKeyDown(KeyCode.A))        
+            inputsPlayer = Inputs.top;           
+        
+        if (Input.GetKeyDown(KeyCode.D))       
+            inputsPlayer = Inputs.top;
+                                 
     }
     private Collider2D CheckIfWallOnX()
     {
@@ -85,5 +103,11 @@ public class GridBasedMovement : MonoBehaviour
         animator.SetFloat("Vertical", yInput);
         animator.SetFloat("Speed", new Vector2(xInput, yInput).sqrMagnitude);
     }    
+    private void SetRotationOfArrow()
+    {
+        Vector2 lookDirection = move;
+        float angle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
+        rigidbodyPlayer.rotation = angle;
+    }
 }
 public enum Inputs { top, right, left, bottom }
--- NSM_ProgTest/Assets/Scripts/Weapon/FireManager.cs
+++ NSM_ProgTest/Assets/Scripts/Weapon/FireManager.cs
@@ -8,14 +8,17 @@ public class FireManager : MonoBehaviour
     [SerializeField] Transform shootPoint;
     [SerializeField] GameObject Arrow;
 
+    [SerializeField] float arrowForce;
     private void Update()
     {
-       // if (Input.GetButtonDown("Fire1"))
-            //Shoot();
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
     }
 
     private void Shoot()
     {
-        Instantiate(Arrow, shootPoint.position, shootPoint.rotation);
+        GameObject arrow = Instantiate(Arrow, shootPoint.position, shootPoint.rotation);
+        Rigidbody2D arrowRB = arrow.GetComponent<Rigidbody2D>();
+        arrowRB.AddForce(shootPoint.right * arrowForce, ForceMode2D.Impulse);
     }
 }
